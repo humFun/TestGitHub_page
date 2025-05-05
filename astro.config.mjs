973405d3d676d6271fb5ae1bbbf1e640230cc5be
KonG Im <zxcasd4144232@gmail.com>
2025-05-06 02:39:54 +0700

    change to astro framework

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://humfun.github.io',
+    base: 'TestGitHub_pag',
+});
